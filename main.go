ee1b7de56ddc67b4c275c2c39bb6615a27e904bc
MOSTAIN BILLAH <bill.rassel@gmail.com>
2021-10-07 13:12:26 +0600

    everything comment out

--- main.go
+++ main.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"time"
-)
-
 type Anytype struct {
 	name string
 }
@@ -29,82 +23,80 @@ func NewAnytype() *AnytypeReader {
 
 func main() {
 
-	fsL, err := os.ReadDir(".") ///home/mostain/go/src/practice
-	if err != nil {
-		fmt.Println(err)
-	}
-	for i, v := range fsL {
-
-		//v.Info()
-		fmt.Println(i, v.IsDir(), v.Name(), v.Type().Perm())
-
-	}
+	// fsL, err := os.ReadDir(".") ///home/mostain/go/src/practice
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// for i, v := range fsL {
+	// 	//v.Info()
+	// 	fmt.Println(i, v.IsDir(), v.Name(), v.Type().Perm())
+	// }
 
 	//open the folder as as file, much faster/efficient solution
-	fd, err := os.Open("..")
-	if err != nil {
-		fmt.Println(err)
-	}
+	// fd, err := os.Open("..")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
 
-	fsList, err := fd.Readdir(1024)
-	if err != nil {
-		fmt.Println(err)
-	}
-	for i, file := range fsList {
-		fmt.Println(i, file.Name(), file.IsDir(), file.Mode().Perm())
-	}
+	// fsList, err := fd.Readdir(1024)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// for i, file := range fsList {
+	// 	fmt.Println(i, file.Name(), file.IsDir(), file.Mode().Perm())
+	// }
 
 	//pta := &Anytype{name: "Mostain"}
 	//pta := new(Anytype)
-	nat := NewAnytype()
+	// nat := NewAnytype()
 
-	fmt.Printf("%T\n", nat)
-	fmt.Println(nat.name, nat.Pp().name, nat.Pp())
+	// fmt.Printf("%T\n", nat)
+	// fmt.Println(nat.name, nat.Pp().name, nat.Pp())
 
 	//3 fallthrough, break how it works
-	tier := 1
-	age := 18
-	switch tier { // switch statement
-	case 1: // case statement
-		fmt.Println("T-shirt")
-		if age < 18 {
-			break // exits the switch block
-		}
-		fallthrough // executes the next case
-	case 2:
-		fmt.Println("Mug")
-		fallthrough // executes the next case
-	case 3:
-		fmt.Println("Sticker pack")
-	default: // executed if no case is satisfied
-		fmt.Println("no reward")
+	// tier := 1
+	// age := 18
+	// switch tier { // switch statement
+	// case 1: // case statement
+	// 	fmt.Println("T-shirt")
+	// 	if age < 18 {
+	// 		break // exits the switch block
+	// 	}
+	// 	fallthrough // executes the next case
+	// case 2:
+	// 	fmt.Println("Mug")
+	// 	fallthrough // executes the next case
+	// case 3:
+	// 	fmt.Println("Sticker pack")
+	// default: // executed if no case is satisfied
+	// 	fmt.Println("no reward")
 
-	}
+	// }
 
 	//example-4 select channel
-	var ch1 = make(chan int)
-	var ch2 = make(chan int)
-	var a, b int
+	// var ch1 = make(chan int)
+	// var ch2 = make(chan int)
+	// var a, b int
 
-	go func() {
-		time.Sleep(1 * time.Microsecond)
-		ch1 <- 10
-	}()
+	// go func() {
+	// 	time.Sleep(1 * time.Microsecond)
+	// 	ch1 <- 10
+	// }()
 
-	go func() {
-		time.Sleep(2 * time.Microsecond)
-		b = <-ch2
-	}()
+	// go func() {
+	// 	time.Sleep(2 * time.Microsecond)
+	// 	b = <-ch2
+	// }()
 
-	select { // the first operation that completes is selected
+	// select { // the first operation that completes is selected
 
-	case a = <-ch1: //receiving from a channel
-		fmt.Println(a)
+	// case a = <-ch1: //receiving from a channel
+	// 	fmt.Println(a)
 
-	case ch2 <- 20: //sending to a channel
-		fmt.Println(b)
-	}
+	// case ch2 <- 20: //sending to a channel
+	// 	fmt.Println(b)
+	// }
 
-	fmt.Println(a, b)
+	// fmt.Println(a, b)
 
 }
