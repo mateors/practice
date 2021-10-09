54346d5801a17aab269bd792afe2ceb36e8d7b01
MOSTAIN BILLAH <bill.rassel@gmail.com>
2021-10-09 22:31:06 +0600

    two dimentional array

--- download/main.go
+++ download/main.go
@@ -7,6 +7,36 @@ import (
 	"os"
 )
 
+func init() {
+
+	//two dimension array
+	var sections = make([][2]int, 10) //declaration & initialization with 0 0 value
+
+	//assign value for the first or index 0
+	sections[0][0] = 10
+	sections[0][1] = 20
+
+	//assign value for the second or index 1
+	sections[1][0] = 30
+	sections[1][1] = 40
+
+	//assign value for the third or index 2
+	sections[2][0] = 50
+	sections[2][1] = 60
+
+	//assign value for the fourth or index 3
+	sections[3][0] = 70
+	sections[3][1] = 80
+
+	//assign value for the fifth or index 4
+	sections[4][0] = 90
+	sections[4][1] = 99
+
+	fmt.Println(sections)
+	//os.Exit(0)
+
+}
+
 type cwriter struct {
 	total uint64
 }
