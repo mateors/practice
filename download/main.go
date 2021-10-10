5bcb2042c1c1f0c4e8ae6a211ab2c4134bd8b43d
MOSTAIN BILLAH <bill.rassel@gmail.com>
2021-10-10 09:47:31 +0600

    google file download try

--- download/main.go
+++ download/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
 )
 
 func init() {
+
+	//r.Header.Set("Range", fmt.Sprintf("bytes=%v-%v", c[0], c[1]))
+
+	//https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Range
+	//Request: Range -> bytes=100-200
+	//Content-Range: bytes 200-1000/67589
+	//vrange := fmt.Sprintf("bytes=%v-%v", 0, 100)
+	//fmt.Println(vrange)
+
+	// clength, err := getContentLength("https://drive.google.com/uc?export=download&confirm=hF9Z&id=1FFM-WG5JSkg0a41ZTEH4oinGJQz5sKsm")
+	// fmt.Println(err, clength)
+	// os.Exit(0)
+
+	downloadmedia("https://doc-10-bk-docs.googleusercontent.com/docs/securesc/ug6gumdndi1jq9dh2rv9sn6k67fcvu0k/0lk3ilt11pnomqpku42h20as680u38o1/1633836975000/04552009210644688537/06512679350586085468Z/1FFM-WG5JSkg0a41ZTEH4oinGJQz5sKsm?e=download&nonce=hl4t1jfcbba0u&user=06512679350586085468Z&hash=9fc30vc21volfs36n1asepurh0uuppro")
+	os.Exit(0)
 
 	size, err := getContentLength("https://automan.biz/resources/images/contact.png")
 	fmt.Println(size, err)
@@ -94,22 +111,36 @@ func twoDimensionalArray() {
 
 func getContentLength(fileUrl string) (uint64, error) {
 
-	req, err := http.NewRequest("HEAD", fileUrl, nil)
+	//HEAD
+	req, err := http.NewRequest("GET", fileUrl, nil)
 	if err != nil {
 		return 0, err
 	}
-	req.Header.Set("User-Agent", "MateorsDownloader")
+	//req.Header.Set("User-Agent", "MateorsDownloader")
+	//req.Header.Set("NIC", "511=uy4ufW5-yIzQPEa_Wm3aa9pgfOp9v_m2yPx-jxn9E7lM5fZ-qRoiVWZaLgQkShxYhhd-rFdSg84lOeeJ9pOk--m7yGK3-NpKynzVZpuBK-t8Xn50vvnydyLICg5eIv8E2zZRaOrGIzl62vSq__QEWmJhM5JuF4vUfglDYnwxJks")
+
+	req.AddCookie(&http.Cookie{Name: "NID", Value: "511=JudNtY18WynKObaxmdN1FPPiD-5E7dwmZQt5WwticA9QTI9iVRDzQj1yMFCQla-MNJqy8VBLBdqPFxgVIBTYI8G6loeqcwjv4UX5-elIvEfnHZPshP5BshRlqZ97tngUimLXBJyS2i7CilxGl5t2pRye9Vl4B2H1oqIYe8NLeLk"})
+	req.AddCookie(&http.Cookie{Name: "download_warning_13058876669334088843_1FFM-WG5JSkg0a41ZTEH4oinGJQz5sKsm", Value: "hF9Z"})
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return 0, err
 	}
 
+	for key, val := range resp.Header {
+		fmt.Println(">>", key, val)
+	}
+
+	fmt.Println("Status-Code:", resp.StatusCode)
 	//verifying is really body empty or not
-	// bs, err := ioutil.ReadAll(resp.Body)
-	// fmt.Println(bs, err)
+	bs, err := ioutil.ReadAll(resp.Body)
+	fmt.Println("***", bs, err)
+
 	length := resp.Header.Get("Content-Length")
-	//fmt.Println(length)
+	fmt.Println("length:", length)
+	if length == "" {
+		return 0, errors.New("no response from content-length header")
+	}
 	return strconv.ParseUint(length, 10, 64)
 }
 
@@ -126,8 +157,17 @@ func (c *cwriter) Write(p []byte) (n int, err error) {
 
 func main() {
 
-	downloadUrl := "https://automan.biz/resources/images/contact.png"
+	// var bs = make([]byte, 100)
+	// n, err := pf.Read(bs)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
 
+}
+
+func downloadmedia(downloadUrl string) {
+
+	//downloadUrl := "https://automan.biz/resources/images/contact.png"
 	resp, err := http.Get(downloadUrl)
 	if err != nil {
 		fmt.Println(err)
@@ -135,12 +175,12 @@ func main() {
 	}
 	defer resp.Body.Close()
 
-	// for key, val := range resp.Header {
-	// 	fmt.Println(key, val)
-	// }
-	// os.Exit(0)
+	for key, val := range resp.Header {
+		fmt.Println("##", key, val)
+	}
+	fmt.Println("StatusCode:", resp.StatusCode)
 
-	pf, err := os.OpenFile("./download/code.png", os.O_CREATE|os.O_WRONLY, 0600)
+	pf, err := os.OpenFile("./download/code", os.O_CREATE|os.O_WRONLY, 0600)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -152,12 +192,5 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-
-	// var bs = make([]byte, 100)
-	// n, err := pf.Read(bs)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
 	fmt.Println("bytes written:", written)
 }
