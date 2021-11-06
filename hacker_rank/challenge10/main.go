cbdcd26bfa171bdfc4d430d823fedc63f278854c
MOSTAIN BILLAH <bill.rassel@gmail.com>
2021-11-06 08:32:03 +0600

    hacker rank challenge 10 paused

---
+++ hacker_rank/challenge10/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+//matrix = {'AA': 'A', 'BD': 'C', 'AC': 'C', 'AB': 'B', 'AD': 'D', 'BB': 'A', 'BC': 'D', 'CC': 'A', 'CB': 'D', 'CA': 'C', 'DB': 'C', 'DC': 'B', 'CD': 'B', 'DA': 'D', 'DD': 'A', 'BA': 'B'}
+
+func pmix(s string, k int32) string {
+	// Write your code here
+
+	return ""
+
+}
+
+func main() {
+
+	var s string
+	var k int32
+
+	res := pmix(s, k)
+	fmt.Println(res)
+
+}
