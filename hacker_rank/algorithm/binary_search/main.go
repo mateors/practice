72f978a6c8ecd627a8b5d2e270341925234ee291
MOSTAIN BILLAH <bill.rassel@gmail.com>
2021-11-11 08:26:47 +0600

    binary search algorithm implementation

---
+++ hacker_rank/algorithm/binary_search/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func binarySearch(array []int32, searchValue int32) int {
+
+	lowerBound := 0
+	upperBound := len(array) - 1
+
+	for lowerBound <= upperBound {
+
+		midpoint := (upperBound + lowerBound) / 2
+		midpointValue := array[midpoint]
+
+		if searchValue == midpointValue {
+			return midpoint
+
+		} else if searchValue < midpointValue {
+			upperBound = midpoint - 1
+
+		} else if searchValue > midpointValue {
+			lowerBound = midpoint + 1
+		}
+	}
+	return -1
+}
+
+func main() {
+
+	//as it is a binary search algorithm so array must be sorted or ordered list
+	array := []int32{3, 17, 75, 80, 202}
+	indxPosition := binarySearch(array, 75)
+	fmt.Println(indxPosition)
+
+}
