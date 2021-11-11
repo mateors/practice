ae9508d4598c89b523aeb3674a2301ff070f0464
MOSTAIN BILLAH <bill.rassel@gmail.com>
2021-11-11 09:33:26 +0600

    calculate number of steps taken func added

--- hacker_rank/algorithm/binary_search/main.go
+++ hacker_rank/algorithm/binary_search/main.go
@@ -26,12 +26,31 @@ func binarySearch(array []int32, searchValue int32) int {
 	return -1
 }
 
+func findNumberOfSteps(arrayCount int32) int {
+
+	var count int
+	for {
+
+		steps := arrayCount / 2
+		arrayCount = steps
+		//fmt.Println(">", steps)
+		if 1 > steps {
+			break
+		}
+		count++
+	}
+	return count
+}
+
 func main() {
 
 	//as it is a binary search algorithm implementation
 	// so array must be sorted or ordered list
-	array := []int32{3, 17, 75, 80, 202}
-	indxPosition := binarySearch(array, 75)
-	fmt.Println(indxPosition)
+	//array := []int32{3, 17, 75, 80, 202}
+	//indxPosition := binarySearch(array, 75)
+	//fmt.Println(indxPosition)
+
+	steps := findNumberOfSteps(100000)
+	fmt.Println("steps:", steps)
 
 }
